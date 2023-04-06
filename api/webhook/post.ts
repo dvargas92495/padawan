1b366be684f7e3b4abd657d14cba211183ead5d4
David Vargas <dvargas92495@gmail.com>
2023-04-06 05:18:42 +0000

    ready to deploy to vargas backend

--- api/webhook/post.ts
+++ api/webhook/post.ts
@@ -1,9 +1,17 @@
 import createAPIGatewayProxyHandler from "@samepage/backend/createAPIGatewayProxyHandler";
 import { Octokit } from "@octokit/rest";
 import jsonwebtoken from "jsonwebtoken";
+import crypto from "crypto";
 
-const logic = async (args: unknown) => {
+const logic = async (args: Record<string, unknown>) => {
+  const computed = crypto
+    .createHash("sha256")
+    .update(process.env.WEBHOOK_SECRET || "")
+    .update(JSON.stringify(args))
+    .digest();
+  const actual = args["x-hub-signature-256"];
   console.log("args", args);
+  console.log("compare hashes", computed, actual);
   const privateKey = process.env.APP_PRIVATE_KEY;
   if (!privateKey) throw new Error(`Missing App Private Key`);
   const octokit = await new Octokit({
@@ -26,4 +34,5 @@ const logic = async (args: unknown) => {
 
 export default createAPIGatewayProxyHandler({
   logic,
+  includeHeaders: ["x-hub-signature-256"],
 });
