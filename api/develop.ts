0ac48e3f210be53f9d8caa3f3fe72d38e8379242
David Vargas <dvargas92495@gmail.com>
2023-04-20 01:55:45 -0400

    Starting to checkout code locally

--- api/develop.ts
+++ api/develop.ts
@@ -5,12 +5,20 @@ import { PromptTemplate } from "langchain/prompts";
 import { Tool } from "langchain/tools";
 import { initializeAgentExecutor } from "langchain/agents";
 import { Octokit } from "@octokit/rest";
+import { v4 } from "uuid";
+import { execSync } from "child_process";
 import getInstallationToken from "../src/utils/getInstallationToken";
 import appClient from "../src/utils/appClient";
 
-class GithubSearchTool extends Tool {
-  name = "Github Search";
-  description = "Searches Github for a given query";
+class GithubCodeSearchTool extends Tool {
+  name = "Github Code Search";
+  description = `Code search looks through the files hosted on GitHub. You can also filter the results:
+- install repo:charles/privaterepo	  Find all instances of install in code from the repository charles/privaterepo.
+- shogun user:heroku	  Find references to shogun from all public heroku repositories.
+- join extension:coffee 	Find all instances of join in code with coffee extension.
+- system size:>1000	  Find all instances of system in code of file size greater than 1000kbs.
+- examples path:/docs/	  Find all examples in the path /docs/.
+- replace fork:true	  Search replace in the source code of forks.`;
   octokit: Octokit;
   constructor({ auth }: { auth?: string }) {
     super();
@@ -19,16 +27,126 @@ class GithubSearchTool extends Tool {
     });
   }
   async _call(input: string) {
-    // need to massage this search. Current input: `dvargas92495/roamjs-smartblocks issue 63 file diffs`
     return this.octokit.search
       .code({
         q: input,
       })
       .then((res) => {
-        if (!res.data.items.length) return "There were no results found";
-        console.log("found", res.data.items.length, "results");
-        return res.data.items[0].sha;
+        return JSON.stringify(res.data);
       });
+  }
+}
+
+class GithubIssueGetTool extends Tool {
+  name = "Github Issue Get";
+  description = `Get an issue from a GitHub repository. Please format your input as a JSON object with the following parameters:
+- owner: (string) [REQUIRED] The account owner of the repository. The name is not case sensitive.
+- repo: (string) [REQUIRED] The name of the repository. The name is not case sensitive.
+- issue_number: (number) [REQUIRED] The number that identifies the issue.`;
+  octokit: Octokit;
+  constructor({ auth }: { auth?: string }) {
+    super();
+    this.octokit = new Octokit({
+      auth,
+    });
+  }
+  async _call(input: string) {
+    return this.octokit.issues
+      .get(JSON.parse(input.trim().replace(/^```/, "").replace(/```$/, "")))
+      .then((res) => {
+        return JSON.stringify(res.data, null, 2);
+      })
+      .catch((e) => {
+        return JSON.stringify(e.response.data);
+      });
+  }
+}
+
+class GithubPullRequestCreateTool extends Tool {
+  name = "Github Pull Request Create";
+  description = `Create a pull request. Please format your input as a JSON object with the following parameters:
+- owner: (string) [REQUIRED] The account owner of the repository. The name is not case sensitive.
+- repo: (string) [REQUIRED] The name of the repository. The name is not case sensitive.
+- title: (string) [REQUIRED] The title of the new pull request. To close a related issue, use the keyword "Closes" followed by the issue number.
+- head: (string) [REQUIRED] The name of the branch where your changes are implemented. Make sure you have changes committed on a separate branch before you create a pull request.
+- base: (string) [REQUIRED] The name of the branch you want the changes pulled into. This should be an existing branch on the current repository. You cannot submit a pull request to one repository that requests a merge to a base of another repository.
+- body: (string) [OPTIONAL] The contents of the pull request.`;
+  octokit: Octokit;
+  constructor({ auth }: { auth?: string }) {
+    super();
+    this.octokit = new Octokit({
+      auth,
+    });
+  }
+  async _call(input: string) {
+    return this.octokit.pulls
+      .create(JSON.parse(input.trim().replace(/^```/, "").replace(/```$/, "")))
+      .then((res) => {
+        return JSON.stringify(res.data, null, 2);
+      })
+      .catch((e) => {
+        return JSON.stringify(e.response.data);
+      });
+  }
+}
+
+class GithubBranchGetTool extends Tool {
+  name = "Github Branch Get";
+  description = `Get a branch from a GitHub repository. Please format your input as a JSON object with the following parameters:
+  - owner: (string) [REQUIRED] The account owner of the repository. The name is not case sensitive.
+  - repo: (string) [REQUIRED] The name of the repository. The name is not case sensitive.
+  - branch: (string) [REQUIRED] The name of the branch. Cannot contain wildcard characters`;
+  octokit: Octokit;
+  constructor({ auth }: { auth?: string }) {
+    super();
+    this.octokit = new Octokit({
+      auth,
+    });
+  }
+  async _call(input: string) {
+    return this.octokit.repos
+      .getBranch(
+        JSON.parse(input.trim().replace(/^```/, "").replace(/```$/, ""))
+      )
+      .then((res) => {
+        return JSON.stringify(res.data, null, 2);
+      })
+      .catch((e) => {
+        return JSON.stringify(e.response.data);
+      });
+  }
+}
+
+class GitCloneRepository extends Tool {
+  name = "Git Clone Repository";
+  description = `Clone a repository from GitHub into a new local directory. Please format your input as a url in thr format https://github.com/[owner]/[repo].`;
+  constructor() {
+    super();
+  }
+  async _call(input: string) {
+    const root = `/tmp/${v4()}`;
+    execSync(`git clone ${input} ${root}`, { stdio: "inherit" });
+    return `Cloned repository into ${root}`;
+  }
+}
+
+class GitNewBranch extends Tool {
+  name = "Git New Branch";
+  description = `Create a new branch in a local repository. Please format your input as a JSON object with the following parameters:
+- branch: (string) [REQUIRED] The name of the branch. Cannot contain wildcard characters
+- root: (string) [REQUIRED] The path to the root of the repository.`;
+  constructor() {
+    super();
+  }
+  async _call(input: string) {
+    const { branch, root } = JSON.parse(
+      input.trim().replace(/^```/, "").replace(/```$/, "")
+    );
+    const cwd = process.cwd();
+    process.chdir(root);
+    execSync(`git checkout -b ${branch}`, { stdio: "inherit" });
+    process.chdir(cwd);
+    return `Successfully created new branch ${branch} in ${root}`;
   }
 }
 
@@ -40,7 +158,7 @@ const zArgs = z.object({
 });
 
 const template =
-  "You are an engineer working on the GitHub repository: {repo_full}. You have just been assigned to issue {issue}, which is titled {title}. Here is some additional information on the task: {body}. What are the file diffs necessary to complete this task?";
+  "You are an engineer working on the GitHub repository: {repo_full}. You have just been assigned to issue {issue}. Create a pull request that will close this issue.";
 const prompt = new PromptTemplate({
   template: template,
   inputVariables: ["repo_full", "issue", "title", "body"],
@@ -54,13 +172,14 @@ const develop: Handler = async (evt: unknown) => {
   // const auth = await getInstallationToken(type, owner);
   const auth = process.env.GITHUB_TOKEN;
   console.log("working on", owner, repo, issue, type, auth);
-  const octokit = new Octokit({ auth });
-  const issueData = await octokit.issues.get({
-    owner,
-    repo,
-    issue_number: issue,
-  });
-  const tools: Tool[] = [new GithubSearchTool({ auth })];
+  const tools: Tool[] = [
+    new GithubCodeSearchTool({ auth }),
+    new GithubIssueGetTool({ auth }),
+    new GithubPullRequestCreateTool({ auth }),
+    new GithubBranchGetTool({ auth }),
+    new GitCloneRepository(),
+    new GitNewBranch(),
+  ];
   const executor = await initializeAgentExecutor(
     tools,
     model,
@@ -71,9 +190,8 @@ const develop: Handler = async (evt: unknown) => {
   const input = await prompt.format({
     repo_full: `${owner}/${repo}`,
     issue: issue,
-    title: issueData.data.title,
-    body: issueData.data.body,
   });
+  console.log("Ready to call");
   return await executor.call({ input });
 };
 
