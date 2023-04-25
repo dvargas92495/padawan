4dda04f89b4d7efe4b2af61f7538ab59018c075f
David Vargas <dvargas92495@gmail.com>
2023-04-25 00:34:31 -0400

    inlining continued

--- api/develop.ts
+++ api/develop.ts
@@ -2,58 +2,477 @@ import type { Handler } from "aws-lambda";
 import { z } from "zod";
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
-import { initializeAgentExecutor } from "langchain/agents";
+import { LLMChain } from "langchain";
 import { Octokit } from "@octokit/rest";
 import { v4 } from "uuid";
 import { execSync, ChildProcess } from "child_process";
 import fs from "fs";
 import getInstallationToken from "../src/utils/getInstallationToken";
 import appClient from "../src/utils/appClient";
-import { CallbackManager, ConsoleCallbackHandler } from "./base.js";
+import { BaseLanguageModel } from "langchain/dist/base_language";
 
-export class SingletonCallbackManager extends CallbackManager {
-    constructor() {
-        super();
+type Generation = {
+  text: string;
+  generationInfo?: Record<string, any>;
+};
+
+type LLMResult = {
+  generations: Generation[][];
+  llmOutput?: Record<string, any>;
+};
+
+type ChainValues = Record<string, any>;
+
+type AgentAction = {
+  tool: string;
+  toolInput: string;
+  log: string;
+};
+
+type AgentFinish = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  returnValues: Record<string, any>;
+  log: string;
+};
+
+type AgentStep = {
+  action: AgentAction;
+  observation: string;
+};
+
+class BaseCallbackHandler {
+  alwaysVerbose: boolean;
+  ignoreLLM: boolean;
+  ignoreChain: boolean;
+  ignoreAgent: boolean;
+  constructor(
+    input: {
+      alwaysVerbose?: boolean;
+      ignoreLLM?: boolean;
+      ignoreChain?: boolean;
+      ignoreAgent?: boolean;
+    } = {}
+  ) {
+    this.alwaysVerbose = false;
+    this.ignoreLLM = false;
+    this.ignoreChain = false;
+    this.ignoreAgent = false;
+    if (input) {
+      this.alwaysVerbose = input.alwaysVerbose ?? this.alwaysVerbose;
+      this.ignoreLLM = input.ignoreLLM ?? this.ignoreLLM;
+      this.ignoreChain = input.ignoreChain ?? this.ignoreChain;
+      this.ignoreAgent = input.ignoreAgent ?? this.ignoreAgent;
     }
-    static getInstance() {
-        if (!SingletonCallbackManager.instance) {
-            SingletonCallbackManager.instance = new SingletonCallbackManager();
-            SingletonCallbackManager.instance.addHandler(new ConsoleCallbackHandler());
-            if (typeof process !== "undefined" &&
-                // eslint-disable-next-line no-process-env
-                process.env.LANGCHAIN_HANDLER === "langchain") {
-                SingletonCallbackManager.instance.addHandler(new LangChainTracer());
-            }
+  }
+  handleLLMStart?(
+    llm: { name: string },
+    prompts: string[],
+    verbose?: boolean
+  ): Promise<void>;
+
+  handleLLMNewToken?(token: string, verbose?: boolean): Promise<void>;
+
+  handleLLMError?(err: Error, verbose?: boolean): Promise<void>;
+
+  handleLLMEnd?(output: LLMResult, verbose?: boolean): Promise<void>;
+
+  handleChainStart?(
+    chain: { name: string },
+    inputs: ChainValues,
+    verbose?: boolean
+  ): Promise<void>;
+
+  handleChainError?(err: Error, verbose?: boolean): Promise<void>;
+
+  handleChainEnd?(outputs: ChainValues, verbose?: boolean): Promise<void>;
+
+  handleToolStart?(
+    tool: { name: string },
+    input: string,
+    verbose?: boolean
+  ): Promise<void>;
+
+  handleToolError?(err: Error, verbose?: boolean): Promise<void>;
+
+  handleToolEnd?(output: string, verbose?: boolean): Promise<void>;
+
+  handleText?(text: string, verbose?: boolean): Promise<void>;
+
+  handleAgentAction?(action: AgentAction, verbose?: boolean): Promise<void>;
+
+  handleAgentEnd?(action: AgentFinish, verbose?: boolean): Promise<void>;
+}
+
+abstract class BaseCallbackManager extends BaseCallbackHandler {
+  abstract setHandlers(handlers: BaseCallbackHandler[]): void;
+  setHandler(handler: BaseCallbackHandler) {
+    return this.setHandlers([handler]);
+  }
+}
+
+class CallbackManager extends BaseCallbackManager {
+  handlers: BaseCallbackHandler[];
+  constructor() {
+    super();
+    this.handlers = [];
+  }
+  async handleLLMStart(
+    llm: { name: string },
+    prompts: string[],
+    verbose = false
+  ) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreLLM && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleLLMStart?.(llm, prompts);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleLLMStart: ${err}`
+            );
+          }
         }
-        return SingletonCallbackManager.instance;
+      })
+    );
+  }
+  async handleLLMNewToken(token: string, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreLLM && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleLLMNewToken?.(token);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleLLMNewToken: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleLLMError(err: Error, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreLLM && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleLLMError?.(err);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleLLMError: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleLLMEnd(output: LLMResult, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreLLM && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleLLMEnd?.(output);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleLLMEnd: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleChainStart(
+    chain: { name: string },
+    inputs: ChainValues,
+    verbose: boolean
+  ) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreChain && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleChainStart?.(chain, inputs);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleChainStart: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleChainError(err: Error, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreChain && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleChainError?.(err);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleChainError: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleChainEnd(output: LLMResult, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreChain && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleChainEnd?.(output);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleChainEnd: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleToolStart(
+    tool: { name: string },
+    input: string,
+    verbose?: boolean
+  ) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreAgent && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleToolStart?.(tool, input);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleToolStart: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleToolError(err: Error, verbose?: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreAgent && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleToolError?.(err);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleToolError: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleToolEnd(output: string, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreAgent && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleToolEnd?.(output);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleToolEnd: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleText(text: string, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (verbose || handler.alwaysVerbose) {
+          try {
+            await handler.handleText?.(text);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleText: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleAgentAction(action: AgentAction, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreAgent && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleAgentAction?.(action);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleAgentAction: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  async handleAgentEnd(action: AgentFinish, verbose: boolean) {
+    await Promise.all(
+      this.handlers.map(async (handler) => {
+        if (!handler.ignoreAgent && (verbose || handler.alwaysVerbose)) {
+          try {
+            await handler.handleAgentEnd?.(action);
+          } catch (err) {
+            console.error(
+              `Error in handler ${handler.constructor.name}, handleAgentEnd: ${err}`
+            );
+          }
+        }
+      })
+    );
+  }
+  addHandler(handler: BaseCallbackHandler) {
+    this.handlers.push(handler);
+  }
+  removeHandler(handler: BaseCallbackHandler) {
+    this.handlers = this.handlers.filter((_handler) => _handler !== handler);
+  }
+  setHandlers(handlers: BaseCallbackHandler[]) {
+    this.handlers = handlers;
+  }
+  static fromHandlers(handlers: BaseCallbackHandler[]) {
+    class Handler extends BaseCallbackHandler {
+      constructor() {
+        super();
+        Object.defineProperty(this, "alwaysVerbose", {
+          enumerable: true,
+          configurable: true,
+          writable: true,
+          value: true,
+        });
+        Object.assign(this, handlers);
+      }
     }
+    const manager = new this();
+    manager.addHandler(new Handler());
+    return manager;
+  }
+}
+
+class ConsoleCallbackHandler extends BaseCallbackHandler {
+  async handleChainStart(chain: { name: string }) {
+    console.log(`Entering new ${chain.name} chain...`);
+  }
+  async handleChainEnd(_output: ChainValues) {
+    console.log("Finished chain.");
+  }
+  async handleAgentAction(action: AgentAction) {
+    console.log(action.log);
+  }
+  async handleToolEnd(output: string) {
+    console.log(output);
+  }
+  async handleText(text: string) {
+    console.log(text);
+  }
+  async handleAgentEnd(action: AgentFinish) {
+    console.log(action.log);
+  }
+}
+
+class SingletonCallbackManager extends CallbackManager {
+  static instance: SingletonCallbackManager | undefined;
+  constructor() {
+    super();
+  }
+  static getInstance() {
+    if (!SingletonCallbackManager.instance) {
+      SingletonCallbackManager.instance = new SingletonCallbackManager();
+      SingletonCallbackManager.instance.addHandler(
+        new ConsoleCallbackHandler()
+      );
+    }
+    return SingletonCallbackManager.instance;
+  }
 }
 export function getCallbackManager() {
-    return SingletonCallbackManager.getInstance();
+  return SingletonCallbackManager.getInstance();
 }
 
-class Tool {
-    constructor(verbose: boolean, callbackManager: CallbackManager) {
-        
-        this.verbose = verbose ?? (callbackManager ? true : getVerbosity
-        this.callbackManager = callbackManager ?? getCallbackManager();
+abstract class Tool {
+  verbose: boolean;
+  callbackManager: CallbackManager;
+  abstract name: string;
+  abstract description: string;
+  returnDirect = false;
+
+  protected abstract _call(arg: string): Promise<string>;
+  constructor(verbose = false, callbackManager?: CallbackManager) {
+    this.verbose = verbose ?? !!callbackManager;
+    this.callbackManager = callbackManager ?? getCallbackManager();
+  }
+  async call(arg: string, verbose = false) {
+    const _verbose = verbose ?? this.verbose;
+    await this.callbackManager.handleToolStart(
+      { name: this.name },
+      arg,
+      _verbose
+    );
+    let result;
+    try {
+      result = await this._call(arg);
+    } catch (e) {
+      await this.callbackManager.handleToolError(e as Error, _verbose);
+      throw e;
     }
-    async call(arg, verbose) {
-        const _verbose = verbose ?? this.verbose;
-        await this.callbackManager.handleToolStart({ name: this.name }, arg, _verbose);
-        let result;
-        try {
-            result = await this._call(arg);
-        }
-        catch (e) {
-            await this.callbackManager.handleToolError(e, _verbose);
-            throw e;
-        }
-        await this.callbackManager.handleToolEnd(result, _verbose);
-        return result;
-    }
+    await this.callbackManager.handleToolEnd(result, _verbose);
+    return result;
+  }
 }
 
+const PREFIX = `Answer the following questions as best you can. You have access to the following tools:`;
+const formatInstructions = (toolNames: string) => `Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [${toolNames}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question`;
+const SUFFIX = `Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}`;
+
+const initializeAgentExecutor = async (
+  tools: Tool[],
+  llm: BaseLanguageModel,
+  _verbose?: boolean,
+  _callbackManager?: BaseCallbackManager
+) => {
+  const verbose = _verbose ?? !!_callbackManager;
+  const callbackManager = _callbackManager ?? getCallbackManager();
+
+  const prefix = PREFIX;
+  const suffix = SUFFIX;
+  const inputVariables = ["input", "agent_scratchpad"];
+  const toolStrings = tools
+    .map((tool) => `${tool.name}: ${tool.description}`)
+    .join("\n");
+  const toolNames = tools.map((tool) => tool.name).join("\n");
+  const instructions = formatInstructions(toolNames);
+  const template = [prefix, toolStrings, instructions, suffix].join("\n\n");
+  const prompt = new PromptTemplate({
+    template,
+    inputVariables,
+  });
+  const chain = new LLMChain({ prompt, llm });
+
+  return AgentExecutor.fromAgentAndTools({
+    agent: new ZeroShotAgent({
+      llmChain: chain,
+      allowedTools: tools.map((t) => t.name),
+    }),
+    tools,
+    returnIntermediateSteps: true,
+    verbose,
+    callbackManager,
+  });
+};
 
 class GithubCodeSearchTool extends Tool {
   name = "Github Code Search";
@@ -423,12 +842,7 @@ const develop: Handler = async (evt: unknown) => {
     new ProcessChDir(),
     new FsListFiles(),
   ];
-  const executor = await initializeAgentExecutor(
-    tools,
-    model,
-    "zero-shot-react-description",
-    true
-  );
+  const executor = await initializeAgentExecutor(tools, model, true);
   console.log("loaded executor");
   const input = await prompt.format({
     repo_full: `${owner}/${repo}`,
