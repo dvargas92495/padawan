5474b83c8ca60d7ad0689aa88033b784892b0be1
David Vargas <dvargas92495@gmail.com>
2023-06-11 11:43:50 -0600

    git rm cache

--- web/urls.py
+++ web/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path('missions/', include('web.missions.urls')),
+    path('tools/', include('web.tools.urls')),
     path('admin/', admin.site.urls),
 ]
